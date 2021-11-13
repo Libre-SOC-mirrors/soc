4e46beb1fb6996ede6151eff68238a9c72e6b5b6
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-11-13 15:08:43 +0000

    update submodule to make ngi pointer router pinouts

--- mkpinmux.sh
+++ mkpinmux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd pinmux
 python2 src/pinmux_generator.py -v -s ls180 -o ls180
-python2 src/pinmux_generator.py -v -s ngi_pointer -o ngi_pointer
+python2 src/pinmux_generator.py -v -s ngi_router -o ngi_router
