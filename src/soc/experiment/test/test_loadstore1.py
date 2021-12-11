c042cfc181ae24f8652256b20558291078d1e5ba
Tobias Platen <tplaten@posteo.de>
2021-12-11 17:14:25 +0100

    typo fix, add missing stop statement to _test_loadstore1_ifetch_multi()

--- src/soc/experiment/test/test_loadstore1.py
+++ src/soc/experiment/test/test_loadstore1.py
@@ -192,11 +192,13 @@ def _test_loadstore1_ifetch_multi(dut, mem):
     # should cope with some addresses being invalid
     addrs = [0x10200,0x10204,10208,10200]
     for addr in addrs:
-        yield from debug(dut, "TODO_fetch_from "+hex(addrs))
+        yield from debug(dut, "TODO_fetch_from "+hex(addr))
         # use the new interface in this test
         yield
         yield
         yield
+
+    wbget.stop = True
 
 def _test_loadstore1_ifetch(dut, mem):
     """test_loadstore1_ifetch
