4e6a15f0a77dd7bfffecf172a56ebc00c7b40163
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-12-13 22:32:24 +0000

    return temporarily to older version of pinmux submodule

--- mkpinmux.sh
+++ mkpinmux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd pinmux
 python2 src/pinmux_generator.py -v -s ls180 -o ls180
-python2 src/pinmux_generator.py -v -s ngi_router -o ngi_router
+# temporary - return to older version of pinmux
+#python2 src/pinmux_generator.py -v -s ngi_router -o ngi_router
