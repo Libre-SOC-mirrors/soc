302cf0617a1efdb3fd7cd7c67abfa6ea2a88a425
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2022-02-10 18:06:20 +0000

    Revert "remove dummy trap pipeline"
    This reverts commit de3765400532be229ed4dd8a1d9fdcf1b4bca0ef.

--- src/soc/fu/trap/pipeline.py
+++ src/soc/fu/trap/pipeline.py
@@ -33,15 +33,13 @@ class TrapBasePipe(ControlBase):
     def __init__(self, pspec):
         ControlBase.__init__(self)
         self.pspec = pspec
-        #self.pipe1 = TrapDummyStages(pspec)
+        self.pipe1 = TrapDummyStages(pspec)
         self.pipe2 = TrapStages(pspec)
-        #pipes = [self.pipe1, self.pipe2]
-        pipes = [self.pipe1]
-        self._eqs = self.connect(pipes)
+        self._eqs = self.connect([self.pipe1, self.pipe2])
 
     def elaborate(self, platform):
         m = ControlBase.elaborate(self, platform)
         m.submodules.pipe1 = self.pipe1
-        #m.submodules.pipe2 = self.pipe2
+        m.submodules.pipe2 = self.pipe2
         m.d.comb += self._eqs
         return m
