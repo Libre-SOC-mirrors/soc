264cc7fd7d7547e1e19424b8f8fd0fbfea29cec5
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2022-02-20 22:17:18 +0000

    weird exception, oe not found in the shiftrot input record
    Revert "separate out shiftrot stages due to size of main stage being so large"
    
    This reverts commit 87d6b84e96fb62cda16cc9f335e34fe15ad6cd97.

--- src/soc/fu/shift_rot/pipeline.py
+++ src/soc/fu/shift_rot/pipeline.py
@@ -4,15 +4,11 @@ from soc.fu.shift_rot.input_stage import ShiftRotInputStage
 from soc.fu.shift_rot.main_stage import ShiftRotMainStage
 from soc.fu.shift_rot.output_stage import ShiftRotOutputStage
 
-class ShiftRotStart(PipeModBaseChain):
+class ShiftRotStages(PipeModBaseChain):
     def get_chain(self):
         inp = ShiftRotInputStage(self.pspec)
-        return [inp]
-
-class ShiftRotStage(PipeModBaseChain):
-    def get_chain(self):
         main = ShiftRotMainStage(self.pspec)
-        return [main]
+        return [inp, main]
 
 
 class ShiftRotStageEnd(PipeModBaseChain):
@@ -25,15 +21,13 @@ class ShiftRotBasePipe(ControlBase):
     def __init__(self, pspec):
         ControlBase.__init__(self)
         self.pspec = pspec
-        self.pipe1 = ShiftRotStart(pspec)
-        self.pipe2 = ShiftRotStage(pspec)
-        self.pipe3 = ShiftRotStageEnd(pspec)
-        self._eqs = self.connect([self.pipe1, self.pipe2, self.pipe3])
+        self.pipe1 = ShiftRotStages(pspec)
+        self.pipe2 = ShiftRotStageEnd(pspec)
+        self._eqs = self.connect([self.pipe1, self.pipe2])
 
     def elaborate(self, platform):
         m = ControlBase.elaborate(self, platform)
         m.submodules.pipe1 = self.pipe1
         m.submodules.pipe2 = self.pipe2
-        m.submodules.pipe3 = self.pipe3
         m.d.comb += self._eqs
         return m
